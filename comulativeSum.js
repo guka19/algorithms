538249ee1b1fa1596d7db4c025f5732f71b723d5
guka19 <22100570@ibsu.edu.ge>
2024-05-17 16:36:43 -0700

    comulative sum

---
+++ comulativeSum.js
@@ -0,0 +1,11 @@
+function comulativeSum(A) {
+    let S = [];
+    S[0] = A[0];
+    for (let i = 1; i < A.length; i++) {
+        S[i] = S[i-1] + A[i];
+    }
+
+    return S;
+}
+
+console.log(comulativeSum([1, 11, 12, 42, 3, 4, 99]));
